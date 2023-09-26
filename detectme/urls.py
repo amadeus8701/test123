901b87e0ae804c0f0f461ce5f14208f671ae7f5a
amadeus8701 <ehdud6495@gmail.com>
2023-09-26 19:23:10 +0900

    allowed host update

--- detectme/urls.py
+++ detectme/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('detectme', views.detectme, name='detectme'),
+    path('video_feed/<int:stream_id>/', views.video_feed, name='video_feed'),
 ]
--- detectme/views.py
+++ detectme/views.py
@@ -1,43 +1,30 @@
 from django.shortcuts import render
 from django.views.decorators import gzip
 from django.http import StreamingHttpResponse
+
 import cv2
-import threading
+
 # Create your views here.
-def home(request):
-    return render(request, "home.html")
+def gen_frames(stream_url, stream_id):
+    camera = cv2.VideoCapture(stream_url)
+    ret, buffer = cv2.imencode('.jpg', frame)
+    frame = buffer.tobytes()
+    yield (b'--frame\r\n'
+   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        (self.grabbed, self.frame) = self.video.read()
-        threading.Thread(target=self.update, args=()).start()
+@csrf_exempt
+def index(request):
+    streams = Stream.objects.all()
 
-    def __del__(self):
-            self.video.release()
+    context = {
+        'streams': streams,
+    }
+    return render(request, 'index.html', context)
 
-    def get_frame(self):
-         image = self.frame
-         _,jpeg = cv2.imencode('.jpg',image)
-         return jpeg.tobytes()
-    
-    def update(self):
-        while True:
-             (self.grabbed, self.frame) = self.video.read()
-
-def gen(camera):
-     while True:
-          frame = camera.get_frame()
-          yield(b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-          
-@gzip.gzip_page
-def detectme(requesr):
-     try:
-          cam = VideoCamera()
-          return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-     except: # This is bad! replace it with proper handling
-          print("에러입니다...")
-          pass
+@csrf_exempt
+def video_feed(request, stream_id):
+    stream = get_stream(stream_id)
+    if stream.name == 'xyz':
+        stream_url = f"rtsp://amadeus:4510471@192.168.219.104:554/stream1"
 
         
